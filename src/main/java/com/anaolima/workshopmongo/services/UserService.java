cd22f387c9daf1a88df21c4224ef4007add65a93
ana olima <anaclaraoliveiralima@yahoo.com.br>
2023-07-03 19:14:51 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/anaolima/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.anaolima.workshopmongo.services;
+
+import com.anaolima.workshopmongo.domain.User;
+import com.anaolima.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}

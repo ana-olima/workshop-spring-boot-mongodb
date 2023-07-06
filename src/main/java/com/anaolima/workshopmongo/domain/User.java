dc07a7b426d72ed86badc435c77bdd11f182250e
ana-olima <anaclaraoliveiralima@yahoo.com.br>
2023-07-05 21:27:41 -0300

    Referenciando os posts do usuario

--- src/main/java/com/anaolima/workshopmongo/domain/User.java
+++ src/main/java/com/anaolima/workshopmongo/domain/User.java
@@ -2,19 +2,24 @@ package com.anaolima.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
 public class User implements Serializable{
 	private static final Long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -51,10 +56,19 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {

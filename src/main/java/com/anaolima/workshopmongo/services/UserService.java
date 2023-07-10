810cd484e447bc5b677778a700cad13576b1eca3
ana-olima <anaclaraoliveiralima@yahoo.com.br>
2023-07-10 17:03:42 -0300

    Consulta com varios criterios

--- src/main/java/com/anaolima/workshopmongo/services/UserService.java
+++ src/main/java/com/anaolima/workshopmongo/services/UserService.java
@@ -4,7 +4,6 @@ import com.anaolima.workshopmongo.domain.User;
 import com.anaolima.workshopmongo.dto.UserDTO;
 import com.anaolima.workshopmongo.repository.UserRepository;
 import com.anaolima.workshopmongo.services.exception.ObjectNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,38 +13,42 @@ import java.util.List;
 public class UserService {
 
 
-	@Autowired
-	private UserRepository repo;
+    @Autowired
+    private UserRepository repo;
 
-	public List<User> findAll(){
-		return repo.findAll();
-	}
-	
-	public User findById(String id) {
-		User user = repo.findById(id).get();
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
-	}
-	public User insert (User obj){
-		return repo.insert(obj);
-	}
-	public void delete(String id){
-		findById(id);
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repo.findById(id).get();
+        if (user == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado");
+        }
+        return user;
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
         repo.deleteById(id);
-	}
-	public User update(User obj) {
-		User newObj = repo.findById(obj.getId()).get();
-		updateData(newObj, obj);
-		return repo.save(newObj);
-	}
+    }
 
-	private void updateData(User newObj, User obj) {
-		newObj.setName(obj.getName());
-		newObj.setEmail(obj.getEmail());
-	}
-	public User fromDTO(UserDTO objDto){
-		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
-	}
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId()).get();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
